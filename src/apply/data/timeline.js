29693df16733948d3f8200df07f3c644e40a5231
Varun Nair <varunnair@berkeley.edu>
2023-08-19 17:48:45 -0700

    Apply Section Content Update 2

--- src/apply/data/timeline.js
+++ src/apply/data/timeline.js
@@ -18,49 +18,49 @@ let Timeline = [{
         linkText: "Meet our Members",
     },
     {
-        date: "Tuesday, August 29th, 8-10 PM PST | TBD",
+        date: "Tuesday, August 29, 8-10 PM PST | TBD",
         title: "Info Session #1",
         description: "Join us at one of our info sessions to get a glimpse into what in means to be part of Net Impact Berkeley and to see what we offer to all majors, backgrounds, and interests. You'll have the opportunity to learn more about our upcoming projects, socials, trainings and external events and to connect with all of our members.",
     },
     {
-        date: "Wednesday, August 30th, 8-10 PM PST | VLSB 2050",
+        date: "Wednesday, August 30, 8-10 PM PST | VLSB 2050",
         title: "Info Session #2 + Case Workshop",
         description: "Info Session #2 is the same as Info Session #1 and will take place from 8-9pm. Feel free to take this opportunity to continue to meet more members of our organization, ask questions, and learn about what NIB can offer you. From 9-10pm we host a case workshop to help walk you through the case portion of our interview and what to expect from the overall interview process. This includes best practices, a sample case, and time to ask questions.",
     },
     {
-        date: "Thursday, August 31st, 8-10 PM PST | VLSB 2050",
+        date: "Thursday, August 31, 8-10 PM PST | VLSB 2050",
         title: "Case Coaching Session",
         description: "Come learn about Net Impact's interview process and how to crush a case interview at our Case Coaching Session. You'll have the chance to work in small groups to solve a real world case and learn how to approach and solve case interview questions.",
     },
     {
-        date: "Friday, September 1st, 1 PM PST",
+        date: "Friday, September 1, 1 PM PST",
         title: "Applications Due",
         description: "Our application consists of a resume drop and a few short answer questions. Please submit your application by 1 PM on Friday if you'd like to apply!",
         link: "https://airtable.com/appZJCBfhJN79Dc23/shrs73X85oN1iDBX2",
         linkText: "Application"
     },
     {
-        date: "Saturday, January 28",
+        date: "Saturday, September 2",
         title: "First Round Interviews",
         description: "Invite Only",
     },
     {
-        date: "Sunday, January 29",
+        date: "Sunday, September 3",
         title: "Meet Net Impact Berkeley",
         description: "Invite Only",
     },
     {
-        date: "Monday, January 30",
+        date: "Monday, September 4",
         title: "Coffee Chats",
         description: "Invite Only",
     },
     {
-        date: "Tuesday, January 31",
+        date: "Tuesday, September 5",
         title: "Second Round Interviews",
         description: "Invite Only",
     },
     {
-        date: "Thursday, February 2",
+        date: "Thursday, September 7",
         title: "Welcome to Net Impact Berkeley",
         description: "Invite Only",
     }
