610347f69d9cc032e0f487fdeaf69c492f3d0683
Varun Nair <varunnair@berkeley.edu>
2023-08-19 17:47:11 -0700

    Apply Section Content Changed

--- src/apply/apply.js
+++ src/apply/apply.js
@@ -22,7 +22,7 @@ import {ReactComponent as DownArrow} from './img/atAGlance/down.svg';
 const Apply = () => {
    const virtualTablingLink = "https://berkeley.zoom.us/j/95663292195";
    const coffeeChatSignUp = "https://airtable.com/shr5tNafnTL2VsY33";
-   const interestForm = "https://airtable.com/shr4rHfs2zBGWg4Lm";
+   const interestForm = "https://airtable.com/appZJCBfhJN79Dc23/shryCqmih6Nbw3f1Q";
    const application = "https://airtable.com/appZJCBfhJN79Dc23/shrs73X85oN1iDBX2";
    const caseCoachingSignUp = "https://berkeley.zoom.us/meeting/register/tJEkdu-urjspGtA68NPJLepuKa-CBfMsTvm3";
    const infosession1 = "https://berkeley.zoom.us/j/99404517995?pwd=Q1ljRTlwb3RBdG9TUFpUTzY0L0UvUT09";
@@ -77,17 +77,17 @@ const Apply = () => {
                    <div className="item">
                        <img src={chat} alt="chat icon" />
                        <h4>Tabling</h4>
-                       <p>Tuesday, 1/17 - Thursday, 1/26</p>
+                       <p>Wednesday, 8/23 - Thursday, 8/31</p>
                        <ul>
-                           <li>From January 17 - 26 anytime 8am-4pm, come say hi on at our table on Sproul or the Glade!</li>
+                           <li>From August 23 - 31, anytime 8am-4pm, come say hi on at our table on Sproul or the Glade!</li>
                            {/* <li>Sign up for a 1:1 with a NIB member sometime January 17th - 26th.</li> */}
-                           <li>Fill out our <a className="underline-magical" href="https://airtable.com/shr4rHfs2zBGWg4Lm" target="_blank" rel="noopener noreferrer">interest form</a>!</li>
+                           <li>Fill out our <a className="underline-magical" href="https://airtable.com/appZJCBfhJN79Dc23/shryCqmih6Nbw3f1Q" target="_blank" rel="noopener noreferrer">interest form</a>!</li>
                        </ul>
                    </div>
                    <div className="item">
                        <img src={network} alt="chat icon" />
                        <h4>Infosessions</h4>
-                       <p>Tuesday, 1/24 - Thursday, 1/26</p>
+                       <p>Tuesday, 8/29 - Thursday, 8/31</p>
                        <ul>
                            <li>Tuesday: Infosession #1</li>
                            <li>Wednesday: Infosession #2 + Case Workshop</li>
@@ -97,10 +97,10 @@ const Apply = () => {
                    <div className="item">
                        <img src={doc} alt="chat icon" />
                        <h4>Applications and Interviews</h4>
-                       <p>Friday 1/27 - Tuesday 1/31</p>
+                       <p>Friday 9/1 - Tuesday 9/5</p>
                        <ul>
-                           <li>Applications are due at 1PM on January 27th.</li>
-                           Apply <a className="underline-magical" href="https://airtable.com/shrVUgAG7OtyduOd2" target="_blank" rel="noopener noreferrer"> here. </a>
+                           <li>Applications are due at 1PM on September 1st.</li>
+                           Apply <a className="underline-magical" href="https://airtable.com/appZJCBfhJN79Dc23/shrs73X85oN1iDBX2" target="_blank" rel="noopener noreferrer"> here. </a>
                            <li>Check out our interview FAQ and tips <a className="underline-magical" href="/nib-app-checklist.pdf" target="_blank" rel="noopener noreferrer"> here! </a></li>
                        </ul>
                    </div>
--- src/apply/data/timeline.js
+++ src/apply/data/timeline.js
@@ -4,39 +4,39 @@
 // import youtube from '../img/youtube.png';
 
 let Timeline = [{
-        date: "January 17th - 26th, 8am - 4pm PST | Sproul/The Glade",
+        date: "August 23rd - 31st, 8am - 4pm PST | Sproul/The Glade",
         title: "Tabling",
         description: "Drop by Sproul and Memorial Glade to learn more about our people, projects, and impact! Take this time to meet our incredible members and to ask any questions about consulting for Net Impact.",
-        link: "https://airtable.com/shr4rHfs2zBGWg4Lm",
+        link: "https://airtable.com/appZJCBfhJN79Dc23/shryCqmih6Nbw3f1Q",
         linkText: "Interest Form",
     },
     {
-        date: "January 17th - 26th, 8am - 6pm PST | Zoom/In Person",
+        date: "August 23rd - 31st, 8am - 6pm PST | Zoom/In Person",
         title: "Coffee Chats",
         description: "Sign up for a 20 minute chat with one of our members anytime to meet someone from our community.",
         link: "https://nib.berkeley.edu/members",
         linkText: "Meet our Members",
     },
     {
-        date: "Tuesday, January 24, 8-10 PM PST | VLSB 2050",
+        date: "Tuesday, August 29th, 8-10 PM PST | TBD",
         title: "Info Session #1",
         description: "Join us at one of our info sessions to get a glimpse into what in means to be part of Net Impact Berkeley and to see what we offer to all majors, backgrounds, and interests. You'll have the opportunity to learn more about our upcoming projects, socials, trainings and external events and to connect with all of our members.",
     },
     {
-        date: "Wednesday, January 25, 8-10 PM PST | Dwinelle 155",
+        date: "Wednesday, August 30th, 8-10 PM PST | VLSB 2050",
         title: "Info Session #2 + Case Workshop",
         description: "Info Session #2 is the same as Info Session #1 and will take place from 8-9pm. Feel free to take this opportunity to continue to meet more members of our organization, ask questions, and learn about what NIB can offer you. From 9-10pm we host a case workshop to help walk you through the case portion of our interview and what to expect from the overall interview process. This includes best practices, a sample case, and time to ask questions.",
     },
     {
-        date: "Thursday, January 26, 8-10 PM PST | VLSB 2050",
+        date: "Thursday, August 31st, 8-10 PM PST | VLSB 2050",
         title: "Case Coaching Session",
         description: "Come learn about Net Impact's interview process and how to crush a case interview at our Case Coaching Session. You'll have the chance to work in small groups to solve a real world case and learn how to approach and solve case interview questions.",
     },
     {
-        date: "Friday, January 27, 1 PM PST",
+        date: "Friday, September 1st, 1 PM PST",
         title: "Applications Due",
         description: "Our application consists of a resume drop and a few short answer questions. Please submit your application by 1 PM on Friday if you'd like to apply!",
-        link: "https://airtable.com/shrVUgAG7OtyduOd2",
+        link: "https://airtable.com/appZJCBfhJN79Dc23/shrs73X85oN1iDBX2",
         linkText: "Application"
     },
     {
