f81eb5b365ebee3141e625649bab59d059ef52db
your name <your emailsdfds>
2024-08-25 10:25:55 -0700

    Updated timeline dates F24

--- src/apply/data/timeline.js
+++ src/apply/data/timeline.js
@@ -4,63 +4,63 @@
 // import youtube from '../img/youtube.png';
 
 let Timeline = [{
-    date: "January 16th - 25th, 8am - 4pm PST | Sproul/Glade",
+    date: "August 28th - September 5th, 8am - 4pm PST | Sproul/Glade",
     title: "Tabling",
     description: "Drop by Sproul and Memorial Glade to learn more about our people, projects, and impact! Take this time to meet our incredible members and to ask any questions about consulting for Net Impact.",
-    link: "https://airtable.com/appK6wRqibWMyzVyQ/shrEJ5ILKfM4PwVly",
+    link: "https://airtable.com/appZNUaqLhwW38hJf/shrzmMZkOfJ0uMPyR",
     linkText: "Interest Form",
 },
 {
-    date: "January 17th - 25th, 8am - 6pm PST | Zoom/In Person",
+    date: "August 28 - September 6th, 8am - 6pm PST | Zoom/In Person",
     title: "Coffee Chats",
     description: "Sign up for a 20 minute chat with one of our members anytime to meet someone from our community.",
     link: "https://nib.berkeley.edu/members",
     linkText: "Meet our Members",
 },
 {
-    date: "Tuesday, January 23rd, 8-10 PM PST | Evans 10",
+    date: "Tuesday, September 3rd, 8-10 PM PST | Room TBA",
     title: "Info Session #1",
     description: "Join us at one of our info sessions to get a glimpse into what it means to be part of Net Impact Berkeley and to see what we offer to all majors, backgrounds, and interests. You'll have the opportunity to learn more about our upcoming projects, socials, trainings and external events and to connect with all of our members.",
 },
 {
-    date: "Wednesday, January 24th, 8-10 PM PST | Dwinelle 145",
+    date: "Wednesday, September 4, 8-10 PM PST | Room TBA",
     title: "Info Session #2 + Case Workshop",
     description: "Info Session #2 is the same as Info Session #1 and will take place from 8-9pm. Feel free to take this opportunity to continue to meet more members of our organization, ask questions, and learn about what NIB can offer you. From 9-10pm we host a case workshop to help walk you through the case portion of our interview and what to expect from the overall interview process. This includes best practices, a sample case, and time to ask questions.",
 },
 {
-    date: "Thursday, January 25th, 8-10 PM PST | VLSB 2050",
+    date: "Thursday, September 5th, 8-10 PM PST | Room TBA",
     title: "Case Coaching Session",
     description: "Come learn about Net Impact's interview process and how to crush a case interview at our Case Coaching Session. You'll have the chance to work in small groups to solve a real world case and learn how to approach and solve case interview questions.",
 },
 {
-    date: "Friday, January 26th, 1 PM PST",
+    date: "Friday, September 6th, 1 PM PST",
     title: "Applications Due",
     description: "Our application consists of a resume drop and a few short answer questions. Please submit your application by 1 PM on Friday if you'd like to apply!",
-    link: "https://airtable.com/appK6wRqibWMyzVyQ/shr6XyxVzly3gHzAm",
+    link: "https://airtable.com/appZNUaqLhwW38hJf/shr2WObqWtUvlWpbp",
     linkText: "Application"
 },
 {
-    date: "Saturday, January 27th",
+    date: "Saturday, September 7th",
     title: "First Round Interviews",
     description: "Invite Only",
 },
 {
-    date: "Sunday, January 28th",
+    date: "Sunday, September 8th",
     title: "Meet Net Impact Berkeley",
     description: "Invite Only",
 },
 {
-    date: "Monday, January 29th",
+    date: "Monday, September 9th",
     title: "Coffee Chats",
     description: "Invite Only",
 },
 {
-    date: "Tuesday, January 30th",
+    date: "Tuesday, September 10th",
     title: "Second Round Interviews",
     description: "Invite Only",
 },
 {
-    date: "Thursday, February 1st",
+    date: "Thursday, September 12th",
     title: "Welcome to Net Impact Berkeley",
     description: "Invite Only",
 }
